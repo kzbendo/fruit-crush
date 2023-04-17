550770ccd9dd882ba88ec71df5985a3d0072c086
Kenneth Bendo <kennethbendo@outlook.com>
2023-04-16 23:39:15 -0700

    added tile movement

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,17 @@
 import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
 import { moveBelowReducer } from "./reducers/moveBelow";
+import { dragEndReducer } from "./reducers/dragEnd";
 
 const initialState: {
   board: string[];
   boardSize: number;
+  squareBeingReplaced: Element | undefined;
+  squareBeingDragged: Element | undefined;
 } = {
   board: [],
   boardSize: 8,
+  squareBeingReplaced: undefined,
+  squareBeingDragged: undefined,
 };
 
 const fruitCrushSlice = createSlice({
@@ -16,6 +21,13 @@ const fruitCrushSlice = createSlice({
     updateBoard: (state, action: PayloadAction<string[]>) => {
       state.board = action.payload;
     },
+    dragStart: (state, action: PayloadAction<any>) => {
+      state.squareBeingDragged = action.payload;
+    },
+    dragDrop: (state, action: PayloadAction<any>) => {
+      state.squareBeingReplaced = action.payload;
+    },
+    dragEnd: dragEndReducer,
     moveBelow: moveBelowReducer,
   },
 });
@@ -24,8 +36,13 @@ export const store = configureStore({
   reducer: {
     fruitCrush: fruitCrushSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-export const { updateBoard, moveBelow } = fruitCrushSlice.actions;
+export const { updateBoard, moveBelow, dragStart, dragDrop, dragEnd } =
+  fruitCrushSlice.actions;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
